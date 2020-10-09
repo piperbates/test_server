48b02fd9523bf112991b6c8e55dc197acc0441e4
Piper <bates.sarah42@gmail.com>
2020-10-09 21:33:38 +0100

    Working server

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express"); //calls express (install with npm install express --save)
-const app = express();
-const PORT = 5000;
 
+const app = express(); //can sometimes be referred to as server. Involks express.
+const PORT = 5000; 
+
+app.get("/", (req, res) =>{ 
+    //When port is run, serve user this data
+    res.json ({ hello: "world" });
+});
 
 app.listen(5000, () => {
     console.log(`*** Server running on http://localhost${PORT} ***`)
-});
+}); 
+
+//api should have 4 oporations: CRUD. Create Read Update Delete.
+//
+//Read: GET request
+
+
+
